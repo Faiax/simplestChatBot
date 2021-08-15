2c1acb7ea57f732b1da47fe1709d4f7b164e0076
Faiax <68774895+Faiax@users.noreply.github.com>
2021-08-15 15:30:45 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from nltk.chat.util import Chat, reflections
+
+pairs = [
+    ['my name is (.*)', ["hi %1. how are you?"]],
+    ['hi i am (.*)', ['hi %1, i am a simplistic chatbot ']],
+    ['hello', ['Hi, I am a simplistic chat bot created by Faiaz']],
+    ['what can you do', ["I am learning the art of shitting properly"]],
+    ['are you stupid', ['Yes, i am stupid. so what? ']],
+    ['who are you', ['I am a bot']],
+    ['what type of bot are you', ['I am a simplistic chat bot created by Faiaz']],
+    ['you are really stupid', ['Good. I appreciate that.']],
+    ['who created you', ['I was created by Faiaz']]
+]
+
+chat = Chat(pairs, reflections)
+chat.converse()
